91aa6558db73ea03eaef125ffe44121b5ef8d48c
opensource-for-freedom <timothydaniel14@yahoo.com>
2025-03-16 09:04:52 -0400

    building signature requests

--- scripts/update_signatures.sh
+++ scripts/update_signatures.sh
@@ -4,6 +4,13 @@
 
 SIGNATURE_DIR="/etc/legion/signatures"
 # GIT_REPO="https://github.com/legion-malware/signatures.git"
+GIT_REPO="https://github.com/Da2dalus/The-MALWARE-Repo.git"
+# Other public malware signature repositories:
+GIT_REPO="https://github.com/stamparm/maltrail.git"
+GIT_REPO="https://github.com/Neo23x0/signature-base.git"
+GIT_REPO="https://github.com/Yara-Rules/rules.git"
+
+SIGNATURE_FILE="/etc/legion/signatures/signatures.txt"
 
 echo "[*] Updating signatures..."
 if [ -d "$SIGNATURE_DIR" ]; then
